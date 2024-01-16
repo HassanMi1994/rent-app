cf4c485b0a9d8ace6d19a718fb1699c6cb60fdca
Hassan <hassan.mi1994@gmail.com>
2024-01-16 11:52:19 +0330

    added basic back-end files

---
+++ back-end/domain/entities/Customer.cs
@@ -0,0 +1,26 @@
+﻿namespace Rent.Entities
+{
+    public class Customer
+    {
+        #region props
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string FatherName { get; set; }
+        public string Mobile { get; set; }
+        public string RefereeName { get; set; }
+        #endregion
+
+        #region create
+        public Customer Create(string name, string lastName, string mobile, string refereeName)
+        {
+            Name = name;
+            LastName = lastName;
+            Mobile = mobile;
+            RefereeName = refereeName;
+            return this;
+        }
+        #endregion
+
+    }
+}
