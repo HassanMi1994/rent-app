032d4fe9a2a1f2d8ddce1e5656c731a878f303e2
Hassan <hassan.mi1994@gmail.com>
2024-01-17 22:17:32 +0330

    added bootstrap icon, and changing languages at runtime in appcomponent.

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { TranslocoPipe } from '@ngneat/transloco';
+import { TranslocoPipe, TranslocoService } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,TranslocoPipe],
+  imports: [CommonModule, RouterOutlet, TranslocoPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'rent-front';
+  transLoco: TranslocoService;
+
+  constructor(transLang: TranslocoService) {
+    this.transLoco = transLang;
+  }
+
+  changeLang(lang: string) {
+    this.transLoco.setActiveLang(lang);
+  }
 }
