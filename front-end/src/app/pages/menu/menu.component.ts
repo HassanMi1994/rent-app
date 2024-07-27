ee47e2fbe01e46f3dab01eeadea2dd2011832ca4
Hassan <hassan.mi1994@gmail.com>
2024-07-27 05:53:36 +0330

    added full-screen menu on small devices... thanks to W3Schools!

--- front-end/src/app/pages/menu/menu.component.ts
+++ front-end/src/app/pages/menu/menu.component.ts
@@ -1,13 +1,16 @@
 import { DOCUMENT, Location } from '@angular/common';
-import { AfterViewInit, Component, Inject } from '@angular/core';
+import { AfterViewInit, Component, HostListener, Inject, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TranslocoPipe, TranslocoService } from '@ngneat/transloco';
 import { UserManagerService } from '../../services/user-manager.service';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
+import { Observable, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [TranslocoPipe, RouterLink],
+  imports: [TranslocoPipe, RouterLink, MenubarModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
@@ -15,10 +18,15 @@ export class MenuComponent implements AfterViewInit {
   isDarkMode: boolean;
   selectedLang: string;
   ls: Storage | undefined;
+  items: MenuItem[5];
+  @ViewChild('mySpan') span: HTMLSpanElement;
+
   constructor(public transLoco: TranslocoService, @Inject(DOCUMENT) document: Document, private location: Location, public userManager: UserManagerService) {
     this.selectedLang = this.transLoco.getDefaultLang().toUpperCase();
     this.ls = document.defaultView?.localStorage;
+
   }
+
   ngAfterViewInit(): void {
     if (this.ls) {
       let langHistory = this.ls.getItem('selectedLang');
@@ -27,6 +35,7 @@ export class MenuComponent implements AfterViewInit {
         this.transLoco.setActiveLang(langHistory!);
       }
     }
+    this.onResize(null);
   }
 
   changeLang(lang: string) {
@@ -39,6 +48,29 @@ export class MenuComponent implements AfterViewInit {
     }
     this.selectedLang = lang.toUpperCase();
     this.transLoco.setActiveLang(lang);
+  }
+
+
+  openNav() {
+    (document.getElementsByClassName('overlay')[0] as HTMLDivElement).style.width = "100%";
+  }
+
+  closeNav() {
+    (document.getElementsByClassName('overlay')[0] as HTMLDivElement).style.width = "0%"
+  }
+
+  public innerWidth: any;
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.innerWidth = window.innerWidth;
+    if (this.innerWidth < 800) {
+      (document.getElementsByClassName('sidebar-menu')[0] as HTMLDivElement).style.display = "block";
+      (document.getElementsByClassName('normal-menu')[0] as HTMLDivElement).style.display = "none";
+    } else {
+      (document.getElementsByClassName('sidebar-menu')[0] as HTMLDivElement).style.display = "none";
+      (document.getElementsByClassName('normal-menu')[0] as HTMLDivElement).style.display = "block";
+
+    }
   }
 
   toggleDarkMode() {
