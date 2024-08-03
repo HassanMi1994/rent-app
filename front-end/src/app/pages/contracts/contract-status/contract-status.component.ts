fcc3827d1400e70317e8c3b1e6d57b7fe2e508dd
Hassan <hassan.mi1994@gmail.com>
2024-08-04 02:11:10 +0330

    added status text to the status badge!

--- front-end/src/app/pages/contracts/contract-status/contract-status.component.ts
+++ front-end/src/app/pages/contracts/contract-status/contract-status.component.ts
@@ -1,11 +1,12 @@
 import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
 import { ContractStatus } from '../../../models/enum/ContractStatus';
 import { Contract } from '../../../models/contract.model';
+import { TranslocoPipe } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-contract-status',
   standalone: true,
-  imports: [],
+  imports: [TranslocoPipe],
   templateUrl: './contract-status.component.html',
   styleUrl: './contract-status.component.scss'
 })
@@ -13,7 +14,9 @@ export class ContractStatusComponent {
   @Input() public contract: Contract
   @ViewChild('indicator') public indicator: HTMLSpanElement;
 
-  jon(){
-  // ContractStatus.
+  @Input() public showStatusText = true;
+
+  jon() {
+    // ContractStatus.
   }
 }
