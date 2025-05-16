089924bb656103dcb5685f106a04d7f61f982be0
Hassan <hassan.mi1994@gmail.com>
2025-05-16 17:40:21 +0330

    check for validation for create customer on create button click

--- front-end/src/app/models/customer.model.ts
+++ front-end/src/app/models/customer.model.ts
@@ -1,4 +1,6 @@
+import { Validator } from "fluentvalidation-ts";
 import { baseModel } from "./base.model";
+import { TranslocoService } from "@ngneat/transloco";
 
 export class Customer extends baseModel {
     id: number;
@@ -8,4 +10,16 @@ export class Customer extends baseModel {
     mobile: string = '';
     address: string = '';
     refereeName: string = '';
+}
+
+export class CustomerValidator extends Validator<Customer> {
+    constructor(transloco: TranslocoService) {
+        super();
+
+        this.ruleFor('fullName').notEmpty().withMessage(transloco.translate('validation.notEmpty'));
+        this.ruleFor('nationalityCode').notEmpty().withMessage(transloco.translate('validation.notEmpty'));
+        this.ruleFor('fatherName').notEmpty().withMessage(transloco.translate('validation.notEmpty'));
+        this.ruleFor('mobile').notEmpty().withMessage(transloco.translate('validation.notEmpty'));
+        this.ruleFor('address').notEmpty().withMessage(transloco.translate('validation.notEmpty'));
+    }
 }
--- front-end/src/app/pages/customer/create-customer/create-customer.component.ts
+++ front-end/src/app/pages/customer/create-customer/create-customer.component.ts
@@ -1,10 +1,10 @@
-import { Component, EnvironmentInjector, EventEmitter } from '@angular/core';
+import { Component, EnvironmentInjector, EventEmitter, ViewChildren } from '@angular/core';
 import { TranslocoPipe, TranslocoService } from '@ngneat/transloco';
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { FormInputComponent } from '../../../utils/form-input/form-input.component';
 import { Router, RouterLink } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { Customer } from '../../../models/customer.model';
+import { Customer, CustomerValidator } from '../../../models/customer.model';
 import { FormsModule } from '@angular/forms';
 import { CustomerService } from '../../../services/customer.service';
 
@@ -18,13 +18,35 @@ import { CustomerService } from '../../../services/customer.service';
 export class CreateCustomerComponent {
   customer: Customer = new Customer();
   transLoco: TranslocoService;
+  @ViewChildren('inputs') inputs:FormInputComponent[];
 
   constructor(private customerService: CustomerService, private router: Router, transLoco: TranslocoService) {
     this.transLoco = transLoco;
   }
 
   create() {
-    this.customerService.create(this.customer);
-    this.router.navigateByUrl('/' + this.transLoco.getActiveLang() + '/customers');
+
+
+    let validation = new CustomerValidator(this.transLoco);
+    let errors = validation.validate(this.customer);
+
+    let keys = Object.keys(errors);
+
+    if (errors) {
+      this.inputs.forEach(element => {
+        if (keys.find(x => x == element.labelName)) {
+          let key = element.labelName as keyof typeof errors;
+          element.setInvalid(errors[key] as string)
+        }
+        else {
+          element.setValid();
+        }
+      })
+    }
+
+    if (keys.length == 0) {
+      this.customerService.create(this.customer);
+      this.router.navigateByUrl('/' + this.transLoco.getActiveLang() + '/customers');
+    }
   }
 }
